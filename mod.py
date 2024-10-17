343560f522f9bb61a923c1c2c9b307bda1b85985
Aj connect <ajiriiviero2021@gmail.com>
2024-10-17 22:12:06 +0200

    new

---
+++ mod.py
@@ -0,0 +1,7 @@
+import math
+import numpy as np
+import pandas as pd
+
+print(math.sqrt(49))
+print(np.__version__)
+print(pd.__version__)
--- test.py
+++ test.py
@@ -23,3 +23,39 @@ def rectangle_info(l, w):
     return a, p
 
 print(rectangle_info(4, 10))
+
+print("+"*30)
+#print(list(range(10)))
+
+x = [23, 49, 3293, 34,48, 392]
+for m in x:
+    print(m * 20, end="f_s ")
+
+for k in range(len(x)):
+    print(x[k] * 20)
+
+print("+"*30)
+# def count(nums):
+#     total = 0
+#     for d in nums:
+#         if d <= 90:
+#             print(d)
+#     return total
+
+# lis1 = [34,58,934,44,94,100,200,403]
+# count(lis1)
+
+#while
+def count(nums):
+    t = 0
+    index = 0
+    while index < len(nums):
+        b = nums[index]
+        if b <= 50:
+            print(b)
+            t += 1
+        index += 1
+    return t
+
+lis1 = [34,58,934,44,94,100,200,403]
+count(lis1)
